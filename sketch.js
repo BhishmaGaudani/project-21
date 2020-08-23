937c4c1043e7e07b2bc8f35d379f6153483c69ed
BhishmaGaudani <67182241+BhishmaGaudani@users.noreply.github.com>
2020-08-23 14:09:47 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,41 @@
+  var wall,thickness
+  var bullet, speed , weight
+
+  function setup() {
+  thickness=random(22,83)
+  wall=createsprite(1200,200,thickness,height/2)
+  speed = random(223,321)
+  weight = random (30,52)}
+
+  function draw(){
+  function hasCollided (lbullet,wall)
+  {}
+
+  bulletRightEdge=bullet.x+bullet.width;
+  wallLeftEdge>=wallLeftEdge;
+  if (bulletRightEdge>=wallLeftEdge)
+  {
+    return true 
+  }
+
+  return false
+
+  }
+
+  if(hasCollided(bullet,wall))
+  {
+    bullet.velocityX=0;
+    var damage = 0.5;weight*speed*speed/(thickness*thickness*thickness)
+if( damage>10)
+{
+  wall.shapeColor = color(255,0,0);
+}
+if( damage<10)
+{
+  wall.shapeColor=color (0,255,0)
+}
+  }
+
+
+
+  
